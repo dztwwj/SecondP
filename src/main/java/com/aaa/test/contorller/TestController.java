6e56c4c2ac84f53b07ce7df220e05edc3e443df1
dztwwj <349921311@qq.com>
2018-12-03 20:29:15 +0800

    Initial commit

---
+++ src/main/java/com/aaa/test/contorller/TestController.java
@@ -0,0 +1,14 @@
+package com.aaa.test.contorller;
+
+/**
+ * className:TestController
+ * discription:
+ * author:LiuQian
+ * createTime:2018-12-03 20:19:25
+ */
+public class TestController {
+
+    public static void main(String[] args) {
+        System.out.println("测试controller");
+    }
+}
